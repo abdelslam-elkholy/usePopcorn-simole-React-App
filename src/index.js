8826840df70a702ed40c8e537f195172243a7658
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-07 01:44:16 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+import StarsRating from "./StarsRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarsRating maxValue={10} />
+    <StarsRating />
   </React.StrictMode>
 );
