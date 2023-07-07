107e9d32447ae2116c268d112a6013854f7bc2b8
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-07 03:13:17 +0300

    update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarsRating maxValue={10} />
-    <StarsRating />
+    <StarsRating size={24} color="red" />
   </React.StrictMode>
 );
