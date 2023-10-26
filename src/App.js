764dc414c82c9991a0cf8a0b41ab5d83268bedbb
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-27 00:29:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -417,13 +417,15 @@ function WatchedSummary({ watched }) {
 function WatchedMoviesList({ watched, onDeleteWatched }) {
   return (
     <ul className="list">
-      {watched.map((movie) => (
-        <WatchedMovie
-          movie={movie}
-          key={movie.imdbID}
-          onDeleteWatched={onDeleteWatched}
-        />
-      ))}
+      {watched &&
+        watched.length > 0 &&
+        watched.map((movie) => (
+          <WatchedMovie
+            movie={movie}
+            key={movie.imdbID}
+            onDeleteWatched={onDeleteWatched}
+          />
+        ))}
     </ul>
   );
 }
